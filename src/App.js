af871fa5ec14c7308dfbb933c71aa44d40803c31
Chris Aquino <chris.aquino@gmail.com>
2018-07-20 10:44:44 -0400

    uses handler in InputBox

--- src/App.js
+++ src/App.js
@@ -16,7 +16,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <InputBox />
+        <InputBox
+          handler={this._handleInput}
+        />
       </div>
     );
   }
--- src/InputBox.js
+++ src/InputBox.js
@@ -1,16 +1,11 @@
 import React from 'react';
 
-let savedText = '';
-
-function processInput(e) {
-  console.log(e.target.value);
-  savedText = e.target.value;
-  console.log(savedText);
-}
-
 const InputBox = (props) => {
   return (
-    <input onChange={processInput} />
+    <input onChange={(e) => {
+      props.handler(e.target.value);
+    }}
+    />
   );
 };
 
